04b0bd403d6fae10684718a42bdb5c9a8ee65780
Justin-Marsland <49326379+Justin-Marsland@users.noreply.github.com>
2019-11-01 18:56:20 -0500

    Started working on boss stuff
    Got started initializing boss attacks, but I still have a lot to do with it

---
+++ Rogue Trial/Assets/Scripts/Combat/Boss/Boss_Logic.cs
@@ -0,0 +1,26 @@
+﻿using NaughtyAttributes;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    private int health = 5;
+    private float attackDelay = 2.0f;
+    private float originalTimeToLive;
+    private float timeToLive = 0.5f;
+    public float speed = 5;
+    [ReorderableList]
+    public List<CustomGCOTypes.CollisionLayerKey> targetLayer = new List<CustomGCOTypes.CollisionLayerKey>();
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
