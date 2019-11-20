4ef2f36f6037f9b5fc89f6e4af3aa871fdf260b8
AlexanderEvans <AlexanderEvans@users.noreply.github.com>
2019-11-20 14:59:13 -0600

    pull scripts

--- Rogue Trial/Assets/Scripts/SceneDataManagement/CrossSceneBoolSO.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "new CrossSceneBool", menuName = "ScriptableObjects/CrossSceneBool")]
-public class CrossSceneBoolSO : ScriptableObject
-{
-    public bool value;
-}
--- Rogue Trial/Assets/Scripts/SceneDataManagement/CrossSceneTransformSO.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "new CrossSceneTransformSO", menuName = "ScriptableObjects/CrossSceneTransformSO")]
-public class CrossSceneTransformSO : ScriptableObject
-{
-    public Transform value;
-}
