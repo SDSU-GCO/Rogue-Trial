1b38b1076cbd9a76cd6a7ad10d5fffd249cc2121
AlexanderEvans <AlexanderEvans@users.noreply.github.com>
2019-11-21 20:19:01 -0600

    Added Dialogue Globals

---
+++ Rogue Trial/Assets/Scripts/Dialogue/DialogueGlobals.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GCODialogueEngine
+{
+    public delegate void DialogueEngineActionCharachterEvent();
+    public struct ActionCharachter
+    {
+        public char? charachter;
+        public DialogueEngineActionCharachterEvent action;
+    }
+    public struct Paragraph
+    {
+        public List<ActionCharachter> chars;
+        public void Load(string str)
+        {
+            for(int i = 0; i<str.Length;i++)
+            {
+
+            }
+        }
+    }
+}
