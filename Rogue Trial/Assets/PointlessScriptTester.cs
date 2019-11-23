6cb276cf308b4d3d2eec82caf6b75d4a1c95a700
AlexanderEvans <AlexanderEvans@users.noreply.github.com>
2019-11-23 01:39:09 -0600

    Made crazy code readable.... and stable

---
+++ Rogue Trial/Assets/PointlessScriptTester.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ByteSheep.Events;
+
+public class PointlessScriptTester : MonoBehaviour
+{
+    [SerializeField]
+    public QuickEvent Event = new QuickEvent();
+    [SerializeField]
+    bool _value=false;
+    public bool Value
+    {
+        get => _value;
+        set => ((_value != (_value=value)) ? Event : null)?.Invoke();
+    }
+
+    [SerializeField]
+    bool toAssign;
+    private void Update()
+    {
+        Value = toAssign;
+    }
+
+    public void Print()
+    {
+        Debug.Log(_value);
+    }
+}
--- Rogue Trial/Assets/Scripts/SceneDataManagement/CrossSceneBoolSO.cs
+++ Rogue Trial/Assets/Scripts/SceneDataManagement/CrossSceneBoolSO.cs
@@ -7,20 +7,41 @@ using ByteSheep.Events;
 public class CrossSceneBoolSO : ScriptableObject
 {
     public QuickEvent Event = new QuickEvent();
-    public bool value;
+    bool _value;
+    //public bool Value
+    //{
+    //    get => _value;
+    //    set => ((_value != (_value = value)) ? Event : null)?.Invoke();
+    //}
     public bool Value
     {
-        get
-        {
-            return value;
-        }
-        set
-        {
-            if (this.value != value)
-            {
-                this.value = value;
-                Event?.Invoke();
-            }
-        }
+        get => _value;
+        set { if (_value != value) /*then*/ { _value = value; /*and*/ Event?.Invoke(); }}
     }
+
+
+    //false
+    //(false) = (false) == (true) => true
+    //false
+    //(false) = (false) == (false) => false
+    //true
+    //(true) = (true) == (true) => true
+    //true
+    //(true) = (true) == (false) => false
+
+    //public bool Value
+    //{
+    //    get
+    //    {
+    //        return _value;
+    //    }
+    //    set
+    //    {
+    //        if (_value != value)
+    //        {
+    //            _value = value;
+    //            Event?.Invoke();
+    //        }
+    //    }
+    //}
 }
