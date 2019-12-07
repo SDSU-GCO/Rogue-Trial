dd80988ad25ff573d39a86952c80d736ffe48166
DevonEvans <39815872+DevonEvans@users.noreply.github.com>
2019-12-07 03:11:10 -0600

    made boss death script and boss fight room

---
+++ Rogue Trial/Assets/Scripts/Boss Logic/BossDataSO.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "bossData", menuName = "CustomSO/Boss", order = 0)]
+public class BossDataSO : ScriptableObject
+{
+
+    public Health health;
+
+    public readonly float SLAM_ATTCD=5.0f;
+
+    public readonly float TREMOR_ATTCD=10.0f;
+
+    public readonly float ROCK_DMG = 1.0f;
+
+    public readonly float SLAM_DMG = 2.0f;
+
+    //public Sprite rock;
+
+
+
+}
