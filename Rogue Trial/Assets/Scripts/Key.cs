9d68027d1bb86e2087efa5d0391e94e8899f503d
AlexanderEvans <AlexanderEvans@users.noreply.github.com>
2019-12-13 18:21:49 -0600

    did stuff

---
+++ Rogue Trial/Assets/Scripts/Key.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    KeyListMBDO keyListMBDO;
+
+    private void OnValidate()
+    {
+        if(Application.isEditor)
+        {
+            if(keyListMBDO==null)
+            {
+                MBDOInitializationHelper mBDOInitializationHelper = default;
+                mBDOInitializationHelper.SetupCardinalSubSystem(this);
+                mBDOInitializationHelper.SetupMBDO(ref keyListMBDO);
+#if UNITY_EDITOR
+                UnityEditor.EditorUtility.SetDirty(this);
+#endif
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (!(keyListMBDO.keys.Contains(this)))
+            keyListMBDO.keys.Add(this);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (keyListMBDO.keys.Contains(this))
+            keyListMBDO.keys.Remove(this);
+    }
+
+    private void OnDisable()
+    {
+        if (keyListMBDO.keys.Contains(this))
+            keyListMBDO.keys.Remove(this);
+    }
+}
---
+++ Rogue Trial/Assets/Scripts/MBDOs/KeyListMBDO.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyListMBDO : MBDataObject
+{
+    public List<Key> keys = new List<Key>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
